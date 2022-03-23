6ade65a859f3bcfcba055ab0d6b01b1dc0c4481b
AmithShubhan <amithshubhan0@gmail.com>
2022-03-23 15:08:55 +0530

    first commit

---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,13 @@
+import React,{useState,useEffect} from 'react';
+
+const Navbar = () => {
+  return (
+    <NavContainer>
+        <NavLogo>
+            <Navheading>SIH</Navheading>
+        </NavLogo>
+    </NavContainer>
+  )
+}
+
+export default Navbar
